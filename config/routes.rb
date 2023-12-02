daa11f0baa3c045c8ca98f9743fbaea98a980dd9
Egyh <yuhamaguchi18@gmail.com>
2023-12-02 23:34:42 +0900

    create edit destroy

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:index, :new, :create, :edit, :update, :create, :destroy]
+  
 end
